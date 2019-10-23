c5eae508d2e6b23dd70a9601d7ea2e09e4574d06
DerriereToi <43401896+guillaumemen@users.noreply.github.com>
2019-10-23 14:18:59 +0200

    Add files via upload

---
+++ 2020-B2-ORM-master/tp1/2/index.js
@@ -0,0 +1,20 @@
+const datasets = require("./datasets.json")
+const reset = '\x1b[0m'
+const bright = '\x1b[1m'
+
+exports = module.exports = {
+  name: "Déménagement",
+  datasets,
+  algo: function (input) {
+    // YOUR CODE BETWEEN HERE
+
+    // AND HERE
+  },
+  verify: function (dataset, output) {
+    if (dataset.output !== output) {
+      throw new Error(`${bright}Got ${output} but expected ${dataset.output}${reset}`)
+    } else {
+      return true
+    }
+  }
+}
